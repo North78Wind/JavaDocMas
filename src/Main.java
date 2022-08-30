67f4958fa06938fd66cf368a17c773a9bbe3cbb2
StafeeV <north78wind@gmail.com>
2022-08-30 18:15:36 -0300

    remast

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,12 @@
 public class Main {
-    public static void main(String[] args) {
-      int client = 250;
-      int refill = 1755;
+    public static void main(String[] orgs) {
+        int client = 250;
+        int refill = 1755;
+        int bonus = refill > 1000 ? refill / 100 : 0;
 
-      int bonus = 0 ;
-      if (refill > 1000) {
-          bonus = refill / 100;
-
-          client = client + refill + bonus;
-
-          System.out.println("На Вашем счету: " + client);
-          System.out.println("Пришло бонусов: " + bonus);
-      }
+        client = client + refill + bonus;
+        System.out.println("На Вашем счету: " + client);
+        System.out.println("Пришло бонусов: " + bonus);
     }
 }
+
